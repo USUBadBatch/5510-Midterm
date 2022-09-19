e43b7fd35badf1391fc97038a4ab1a85ef6d70df
Braxton Geary <b.geary01@gmail.com>
2022-09-19 13:06:43 -0600

    Further testing and research

--- problems/p1/main.py
+++ problems/p1/main.py
@@ -9,21 +9,38 @@ CIRCLE_DIAMETER = 5 # in meters
 SKIDSTEER_LEN = .75 # in meters
 SKIDSTEER_WIDTH = .55 # still in meters
 
+# this is the maximum distance that the center of the robot 
+# can get from the center of the circle. Anything more than this
+# and the robot is outside the circle.
+max_dist = (CIRCLE_DIAMETER / 2) - (SKIDSTEER_WIDTH / 2)
+
 skidsteer = Skidsteer(SKIDSTEER_LEN, SKIDSTEER_WIDTH)
 coords: list[tuple[float, float, float]] = []
 
 def get_v_left(elapsed_time: float):
-    return min(14, max(.5, 10*elapsed_time))
+    return min(12, 2*elapsed_time)
 
 def get_dist(x:float, y: float):
     return math.sqrt(x*x + y*y)
 
+'''
+Variables needed:
+total_time = time duration 
+time_coeff = time coefficient
+max_v_left = maximum speed of left
+V_RIGHT = constant velocity of right tread
+'''
+# TODO look at Archimedean spiral 
 
-for t in range(int(1/DELTA_TIME)):
-    coords.append((skidsteer.get_xpos(), skidsteer.get_ypos(), skidsteer.get_theta()))
-    if get_dist(skidsteer.get_xpos, skidsteer.get_ypos) > (CIRCLE_DIAMETER / 2) - (SKIDSTEER_WIDTH / 2):
+for t in range(0, 100):
+    x_pos = skidsteer.get_xpos()
+    y_pos = skidsteer.get_ypos()
+    if get_dist(x_pos, y_pos) > max_dist:
+        print("Too far!")
         break
-    skidsteer.move(get_v_left(t), 16, DELTA_TIME)
+    coords.append((x_pos, y_pos, skidsteer.get_theta()))
+    skidsteer.move(get_v_left(t/10), 16, DELTA_TIME)
+    
 
 plt.plot([x for (x,y,t) in coords], [y for (x,y,t) in coords])
 plt.xlabel("X")
