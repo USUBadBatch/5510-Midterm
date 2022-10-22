1d7259207e701afdf1a465f1c4e3ccf05a46d780
Hadley Hamar <hadley.hamar@aggiemail.usu.edu>
2022-10-22 12:59:52 -0600

    Make the CartpoleEnv accept carts of various sizes

--- problems/p4/cartpole.py
+++ problems/p4/cartpole.py
@@ -53,13 +53,13 @@ class CartPoleEnv(gym.Env):
 
     metadata = {"render.modes": ["human", "rgb_array"], "video.frames_per_second": 50}
 
-    def __init__(self):
+    def __init__(self, cart_mass, pole_mass, pole_length):
         self.gravity = 9.8
-        self.masscart = 0.2
-        self.masspole = 4.0
-        self.total_mass = self.masspole + self.masscart
-        self.length = 0.5  # actually half the pole's length
-        self.polemass_length = self.masspole * self.length
+        self.cart_mass = cart_mass
+        self.pole_mass = pole_mass
+        self.total_mass = self.pole_mass + self.cart_mass
+        self.length = pole_length / 2  # actually half the pole's length
+        self.pole_mass_length = self.pole_mass * self.length
         self.force_mag = 10.0
         self.tau = 0.02  # seconds between state updates
         self.kinematics_integrator = "euler"
@@ -105,12 +105,12 @@ class CartPoleEnv(gym.Env):
         # For the interested reader:
         # https://coneural.org/florian/papers/05_cart_pole.pdf
         temp = (
-            force + self.polemass_length * theta_dot ** 2 * sintheta
+            force + self.pole_mass_length * theta_dot ** 2 * sintheta
         ) / self.total_mass
         thetaacc = (self.gravity * sintheta - costheta * temp) / (
-            self.length * (4.0 / 3.0 - self.masspole * costheta ** 2 / self.total_mass)
+            self.length * (4.0 / 3.0 - self.pole_mass * costheta ** 2 / self.total_mass)
         )
-        xacc = temp - self.polemass_length * thetaacc * costheta / self.total_mass
+        xacc = temp - self.pole_mass_length * thetaacc * costheta / self.total_mass
 
         if self.kinematics_integrator == "euler":
             x = x + self.tau * x_dot
