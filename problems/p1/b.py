f0055c75260b596c08c9e46b9248ca8304fde373
Adam Alder <atariq11700@gmail.com>
2022-10-25 14:06:19 -0600

    finished 1

--- problems/p1/b.py
+++ problems/p1/b.py
@@ -1,6 +1,7 @@
 import math
 from utils import distance, Circle, law_of_cos
 import matplotlib.pyplot as plt
+import numpy as np
 
 from ackermann import Ackermann
 
@@ -25,13 +26,11 @@ def main():
     car.move(AVERAGE_VELOCITY, 0, DELTA_TIME)
     coords.append((car.get_xpos(), car.get_ypos()))
     
-    
     next_pos = None
     count = 0
     alpha = 0
     while True:
         alpha: float = (math.pi / 2.5) - (DELTA_TIME * count)
-
 
         count += 1
         next_pos = car.calc_next_pos(AVERAGE_VELOCITY, alpha, DELTA_TIME)
@@ -46,10 +45,6 @@ def main():
     next_point = (next_pos[0], next_pos[1])
     curr_point = (car.get_xpos(), car.get_ypos())
 
-    circle_cur = plt.Circle((curr_point), .8, fill=False) #type: ignore
-
-
-
     possible_solutions = Circle.find_intersection_points(Circle(CIRCLE_RADIUS, CIRCLE_X, CIRCLE_Y), Circle(AVERAGE_VELOCITY * DELTA_TIME, curr_point[0], curr_point[1]))
     ps1_distance = distance(next_point, possible_solutions[0])
     ps2_distance = distance(next_point, possible_solutions[1])
@@ -60,41 +55,32 @@ def main():
     else:
         solution = possible_solutions[1]
 
-    #use law of cos to find new delta theta
     delta_theta = law_of_cos(distance(curr_point, next_point), distance(curr_point, solution), distance(solution, next_point))
     car.increment_theta(delta_theta + car.calc_delta_theta(AVERAGE_VELOCITY, alpha) * DELTA_TIME)
 
     car.move(AVERAGE_VELOCITY, 0, DELTA_TIME)
     coords.append((car.get_xpos(), car.get_ypos()))
-
-
-
-    alpha = car.calc_alpha_for_inst_turning_raidus(2.5)
-    a_new = math.atan(car.get_ypos() / car.get_xpos()) + (math.pi) 
-    car.set_theta(a_new)
-    car.increment_theta((-car.calc_delta_theta(AVERAGE_VELOCITY, alpha) *DELTA_TIME) / 2)
-
-
-    xp = car.get_xpos()
-    yp = car.get_ypos()
-    coords.append((xp, yp))
-    # plt.plot([CIRCLE_X, xp], [CIRCLE_Y, yp])
-    v1 = (xp - CIRCLE_X, yp - CIRCLE_Y)
     
+    #todo: set new heading
+    alpha = car.calc_alpha_for_inst_turning_raidus(2.5)
+    a_new = math.atan(car.get_ypos() / car.get_xpos()) 
+
+    if a_new > 0:
+        a_new -= math.pi
+
+    a_new += math.pi
+    car.set_theta(a_new)
+    car.increment_theta((-car.calc_delta_theta(AVERAGE_VELOCITY, alpha) * DELTA_TIME) / 2)
 
     alpha = car.calc_alpha_for_inst_turning_raidus(2.5)
     for i in range(int((2 * math.pi * CIRCLE_RADIUS) / (AVERAGE_VELOCITY * DELTA_TIME)) + 1):
         car.move(AVERAGE_VELOCITY, alpha, DELTA_TIME)
-
-
         coords.append((car.get_xpos(), car.get_ypos()))
-
 
     plt.plot([x for (x, y) in coords], [y for (x, y) in coords], color="red")
     plt.scatter(CIRCLE_X, CIRCLE_Y, color="blue")
     circle_main = plt.Circle((CIRCLE_X, CIRCLE_Y), CIRCLE_RADIUS, color='b', fill=False)  # type: ignore
     plt.gca().add_patch(circle_main)
-    # plt.gca().add_patch(circle_cur)
     plt.gca().set_aspect("equal")
     plt.show()
 
