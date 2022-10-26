1750a091094a9be8423e8c81f1fd11ec871285c0
Hadley Hamar <hadley.hamar@aggiemail.usu.edu>
2022-10-26 00:27:05 -0600

    Refactor the program where only the CartPoleController class and OS setup are in cartpole.py. The rendering loop is in the solution.ipynb notebook

--- problems/p4/controller.py
+++ problems/p4/controller.py
@@ -10,14 +10,6 @@ if sys.platform == 'win32':
 elif sys.platform == 'linux' or sys.platform == 'linux2':
     os.environ['SDL_VIDEODRIVER'] = 'dummy'
 
-# env = CartPoleEnv(cart_mass=4.0, pole_mass=0.2, pole_length=1.0)
-env = gym.make('CartPole-v1', render_mode='human')
-env.cart_mass = 4.0
-env.pole_mass = 0.2
-env.length = 1.0 / 2
-env.force_mag = 6.0
-env.theta_threshold_radians = 24 * 2 * math.pi / 360
-
 class CartPoleController():
     def __init__(self, env):
         self.__env = env
@@ -31,12 +23,3 @@ class CartPoleController():
         
         if terminated or truncated:
             self.__observation, _ = self.__env.reset()
-            
-            
-controller = CartPoleController(env)
-
-for _ in range(1000):
-    env.render()
-    controller.next_step()
-    
-env.close()
