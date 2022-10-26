e2feb83950a90c3262f1e38ea1475d3306c8f70f
Adam Alder <atariq11700@gmail.com>
2022-10-26 17:09:33 -0600

    added 6 a and b

---
+++ problems/p6/a.bat
@@ -0,0 +1,3 @@
+@echo off
+start cmd.exe /k python -c "from utils import process_video;process_video('Video_One.mp4')"
+start cmd.exe /k python -c "from utils import process_video;process_video('Video_Two.mp4')"
