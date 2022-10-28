547717475e8e2cf48176859f38400079efe41b05
Adam Alder <atariq11700@gmail.com>
2022-10-27 18:48:01 -0600

    changed stuff

--- problems/p8/b.py
+++ problems/p8/b.py
@@ -1,2 +1,13 @@
 # https://pytorch.org/hub/ultralytics_yolov5/
 
+import torch
+import os
+
+
+
+model = torch.hub.load("ultralytics/yolov5", "custom", path="data/runs/yolov5/weights/best.pt")
+images = os.listdir("data/test")
+
+results = model("D:\\Documents\\code\\school\\cs5510\\5510-Midterm\\problems\\p8\\data\\test\\images\\crop_1.jpeg")
+
+results.save()
